d58063a0d05d86b6f428f8ae0788ffb97f85eaa1
shalupenb <wemlirz35@gmail.com>
2024-09-02 17:29:58 +0300

    added chalk

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 2) ES modules (export-> import)
 */
 //const User = require("./User");
-import User from "./User.js"
+import User from "./User.js";
+import chalk from "chalk";
 const user = new User("Alex", 22);
-console.log(user.toString());
+console.log(chalk.green(user.toString()));
