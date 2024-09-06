e4286fdaf420226de2bd27c485e8607e8bea26ed
shalupenb <wemlirz35@gmail.com>
2024-09-06 10:33:00 +0300

    commit

--- less_1/index.js
+++ less_1/index.js
@@ -4,9 +4,9 @@
 */
 //const User = require("./User");
 import User from "./User.js";
-import Player from "gica-module";
+import Player from "gica-its-module";
 import chalk from "chalk";
 const user = new User("Alex", 22);
-const player = new User("shalupenb", 100);
+const player = new Player("shalupenb", 100);
 console.log(chalk.red(player.toString()));
 console.log(chalk.green(user.toString()));
