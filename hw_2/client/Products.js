4d3fad6cbf4f3eeb050d81558b5bc820b1066880
shalupenb <wemlirz35@gmail.com>
2024-09-14 22:28:29 +0300

    commit

--- hw_2/client/Products.js
+++ hw_2/client/Products.js
@@ -1,5 +1,11 @@
 export default class Products {
   #_link = "http://localhost:3000/products";
+  #_name;
+  #_price;
+  constructor(name,price) {
+    this.#_name = name;
+    this.#_price = price;
+  }
   get() {
     fetch(this.#_link, {
       method: "GET",
@@ -8,13 +14,52 @@ export default class Products {
       .then((res) => console.table(res))
       .catch();
   }
-  post() {
+  post(productData) {
+    const { name, price } = productData;
+    
+    if (!name || !price) {
+      console.error('Name and price are required');
+      return;
+    }
 
+    fetch(this.#_link, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, price }),  // Send only required fields: name, price
+    })
+      .then((data) => data.json())
+      .then((res) => console.log("Product created:", res))
+      .catch();
   }
-  put() {
+  
+  put(productId, updatedData) {
+    const { name, price } = updatedData;
 
+    if (!name || !price) {
+      console.error('Name and price are required');
+      return;
+    }
+
+    fetch(`${this.#_link}/${productId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, price }),
+    })
+      .then((data) => data.json())
+      .then((res) => console.log("Product updated:", res))
+      .catch();
   }
-  delete() {
 
+  delete(productId) {
+    fetch(`${this.#_link}/${productId}`, {
+      method: "DELETE",
+    })
+      .then((data) => data.json())
+      .then((res) => console.log("Product deleted:", res))
+      .catch();
   }
 }
--- hw_2/client/index.js
+++ hw_2/client/index.js
@@ -1,3 +1,17 @@
 import Products from "./Products.js";
 const product = new Products();
+const data = {name: "smth", price: 1000};
+
+//GET
 console.log(product.get());
+
+//POST
+
+//product.post(data);
+
+//PUT
+//product.put('d05a', {name: "SMTH"});
+//product.put('d05a', {price: 1001});
+
+//DELETE
+//product.delete('d05a');
